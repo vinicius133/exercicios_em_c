02bde7cd1cac323efc1dd48f33557f0df080582e
Vinícius <viniciusqs13@gmail.com>
2026-03-26 09:19:00 -0300

    sistema de batalha

---
+++ arena.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    int jogador = 100;
+    int monstro = 80;
+
+    int escolha;
+    while (jogador > 0 && monstro > 0){
+        printf("Qual sua ação?\n 1-Atacar\n 2-Curar");
+        scanf("%d", &escolha);
+
+        if (escolha = 1){
+            monstro = monstro - (rand() % 11 + 10);
+        } else if (escolha = 2){
+            jogador = jogador + (rand() % 11 + 5);
+        }
+
+        jogador = jogador - (rand() % 13 + 6);
+
+
+    }
+    
+    if (jogador > monstro){
+        printf("O Jogador Venceu!");
+    } else{
+        printf("O Monstro Venceu");
+    }
+
+    return 0;
+}
