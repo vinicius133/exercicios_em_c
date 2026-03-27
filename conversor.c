fa88b4eae65215fbdfcc8e39fd5652194b96f869
Vinícius <viniciusqs13@gmail.com>
2026-03-27 12:37:09 -0300

    conversor iniciado

---
+++ conversor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    int escolha;
+    printf("Qual conversão você quer fazer?\nDigite 1 para converter de decimal para binário\nDigite 2 para converter de binário para decimal\n");
+    scanf("%d", &escolha);
+
+    if(escolha == 1){
+
+    } else if(escolha == 2){
+        printf("");
+    }
+}
