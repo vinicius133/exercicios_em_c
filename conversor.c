9c574061eae0416601a4f939e157a9dc50dda25c
Vinícius <viniciusqs13@gmail.com>
2026-03-27 12:44:56 -0300

    conversor escolha 2

--- conversor.c
+++ conversor.c
@@ -6,12 +6,16 @@ int main() {
     SetConsoleCP(65001);
 
     int escolha;
+    int binario_entrada[4];
     printf("Qual conversão você quer fazer?\nDigite 1 para converter de decimal para binário\nDigite 2 para converter de binário para decimal\n");
     scanf("%d", &escolha);
 
     if(escolha == 1){
 
+ 
     } else if(escolha == 2){
-        printf("");
+        printf("Digite o número binário: ");
+        scanf("%d", &binario_entrada);
+        printf(binario_entrada);
     }
 }
