54626b56c2cc14a30e76fbfe9efb2c3cd3eba43f
Vinícius <viniciusqs13@gmail.com>
2026-03-30 15:19:34 -0300

    idade 2

---
+++ atividades_em_sala/idade2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    int idade, diferenca;
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+    if (idade >= 18){
+        diferenca = idade - 18;
+        printf("Já tem habilitação há %d anos!\n", &diferenca);
+    } else{
+        diferenca = 18 - idade;
+        printf("Espere mais %d anos\n", diferenca);
+    }
+
+    return 0;
+}
