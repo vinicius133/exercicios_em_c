eedf650012154e5cc3fb2b80f9a71161ffbe032d
Vinícius <viniciusqs13@gmail.com>
2026-04-01 11:35:18 -0300

    conversor continuado

--- conversor_nf.c
+++ conversor_nf.c
@@ -5,13 +5,21 @@ int main() {
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    int escolha;
+    int escolha, decimal_in;
+    int n = decimal_in;
     int binario_entrada[4];
     printf("Qual conversão você quer fazer?\nDigite 1 para converter de decimal para binário\nDigite 2 para converter de binário para decimal\n");
     scanf("%d", &escolha);
 
     if(escolha == 1){
- 
+        printf("Digite o número de base decimal: ");
+        scanf("%d", &decimal_in);
+
+        
+        
+
+
+
     } else if(escolha == 2){
        
     }
