9008e3b647f7c029dae6e39e8fad00a421cd0217
Vinícius <viniciusqs13@gmail.com>
2026-04-01 11:09:06 -0300

    ordenador continuado

--- ordenar_nf.c
+++ ordenar_nf.c
@@ -5,14 +5,25 @@ int main() {
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    int a, b, c;
+    int a, b, c, temp;
     printf("Digite os 3 números para ordenar: ");
     scanf("%d %d %d", &a, &b, &c);
+    printf("A: %d\nB: %d\nC: %d\n", a, b, c);
 
-    if (a <= b && a <= c){
-        
-    } else
+    if (a > b){
+        a = temp;
+        a = b;
+        b = temp;
+    } else if(a > c){
+        a = temp;
+        a = c;
+        c = temp;
+    } else if(b > c){
+        b = temp;
+        b = c;
+        c = temp;
+    }
+    printf("A: %d\nB: %d\nC: %d\n", a, b, c);
 
     return 0;
 }
-// NÃO TERMINADO
