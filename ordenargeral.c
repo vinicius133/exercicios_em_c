a63bc91d9b242029766985284e64100b42e2dfe2
Vinícius <viniciusqs13@gmail.com>
2026-04-01 11:59:19 -0300

    ordenar geral idealizado com laço for dentro de laço for

--- ordenargeral.c
+++ ordenargeral.c
@@ -5,7 +5,15 @@ int main() {
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
     
-    int n, numeros[n], temp;
+    printf("Digite os números que serão ordenados");
+    scanf("");
+
+    for (int i = 0; i < 4; i++) {
+    for (int j = i + 1; j < 4; j++) {
+        if (vetor[i] > vetor[j]) { 
+            temp = vetor[i];       
+            vetor[i] = vetor[j];
+            vetor[j] = temp;
 
 
     return 0;
