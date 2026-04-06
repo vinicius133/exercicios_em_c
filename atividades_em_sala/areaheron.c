43f14da5469e374af7517c2c4558abb80f29fa71
Vinícius <viniciusqs13@gmail.com>
2026-04-06 16:26:36 -0300

    area heron terminado

--- atividades_em_sala/areaheron.c
+++ atividades_em_sala/areaheron.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <windows.h>
+#include <math.h>
 
 int main() {
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
     float a, b, c;
-    double A;
+    double A, s;
     printf("Digite o valor do lado A, B e C do triângulo: ");
     scanf("%f %f %f", &a, &b, &c);
 
+    s = (a + b + c) / 2;
 
+    A = sqrt(s * (s - a) * (s - b) * (s - c));
+
+    printf("A área é: %lf", A);
 
     return 0;
 }
