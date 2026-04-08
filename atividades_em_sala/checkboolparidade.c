26c8c52f0bb5c4a457a0e3bd41cb14775b44eee5
Vinícius <viniciusqs13@gmail.com>
2026-04-08 14:30:05 -0300

    checador booleano de condições

--- atividades_em_sala/checkboolparidade.c
+++ atividades_em_sala/checkboolparidade.c
@@ -8,7 +8,12 @@ int main() {
     int x; 
     printf("Digite um número inteiro: ");
     scanf("%d", &x);
-    
+
+    if ( ((x % 2 == 0) && (x > 10)) || ((x % 2 != 0) && (x < 50)) ){
+        printf("SIM");
+    } else{
+        printf("NÃO");
+    }
 
     return 0; 
 }
