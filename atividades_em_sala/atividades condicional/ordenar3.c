58e0baed128d3a9d3f7c33bf555e4039d86ff4a8
Vinícius <viniciusqs13@gmail.com>
2026-04-09 22:08:37 -0300

    programa que ordena 3 números em ordem crescente para atividade

---
+++ atividades_em_sala/atividades condicional/ordenar3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    int a, b, c, temp;
+    printf("Digite os 3 números para ordenar: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b){
+        temp = a;
+        a = b;
+        b = temp;
+    } 
+    if(a > c){
+        temp = a;
+        a = c;
+        c = temp;
+    } 
+    if(b > c){
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    printf("A ordem ficou:\nA: %d\nB: %d\nC: %d\n", a, b, c);
+
+    return 0;
+} //nesse usei int para variar, também não tem especificação na questão 
