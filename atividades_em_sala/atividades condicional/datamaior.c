64fe09c46997a0870610b3dfb69dc101ab028185
Vinícius <viniciusqs13@gmail.com>
2026-04-10 22:07:06 -0300

    programa que determina a data cronologicamente maior de duas datas fornecidas pelo usuário iniciado

---
+++ atividades_em_sala/atividades condicional/datamaior.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    int d1, m1, a1, d2, m2, a2;
+    printf("Digite a primeira data separada por espaços (ex: 23 09 2007): ");
+    scanf("%d %d %d", &d1, &m1, &a1);
+
+    printf("Digite a segunda data separada por espaços (ex: 23 09 2007): ");
+    scanf("%d %d %d", &d2, &m2, &a2);
+
+    if (a1 > a2){
+        printf("A primeira data é maior!\n");
+    } else if (a1 < a2){
+        
+    }
+
+    return 0;
+}
