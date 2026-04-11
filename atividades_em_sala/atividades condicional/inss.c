68e16df0413fe01181233b2818e76afbcae4cf9c
Vinícius <viniciusqs13@gmail.com>
2026-04-10 22:46:38 -0300

    programa teste condicional com inss

--- atividades_em_sala/atividades condicional/inss.c
+++ atividades_em_sala/atividades condicional/inss.c
@@ -1,9 +1,9 @@
 // Uma pessoa pode se aposentar pelo INSS caso esteja em alguma das situações
 // abaixo:
 // - É do sexo masculino, possui pelo menos 65 anos, e pelo menos 10 anos de
-// contribuição.
+// contribuição. //////////////////////////////////////////////////////
 // - É do sexo masculino, possui pelo menos 63 anos, e pelo menos 15 anos de
-// contribuição.
+// contribuição. //////////////////////////////////////////////////////
 // - É do sexo feminino, possui pelo menos 63 anos, e pelo menos 10 anos de
 // contribuição.
 
@@ -20,7 +20,7 @@ int main() {
     char sexo;
     int idade, cont;
     printf("Digite o seu sexo(m para masculino e f para feminino): ");
-    scanf("%c", &sexo);
+    scanf(" %c", &sexo);
 
     printf("Digite a sua idade: ");
     scanf("%d", &idade);
@@ -29,8 +29,21 @@ int main() {
     scanf("%d", &cont);
 
     if (sexo == 'm'){
-        
+        if (idade >= 65 && cont >= 10 || idade >= 63 && cont >= 15){
+            printf("Você pode se aposentar pelo INSS!\n");
+        } else{
+            printf("Você não pode se aposentar pelo INSS!\n");
+        }
+    } else if (sexo == 'f'){
+        if (idade >= 63 && cont >= 10 || idade >= 61 && cont >= 15){
+            printf("Você pode se aposentar pelo INSS!\n");
+        } else{
+            printf("Você não pode se aposentar pelo INSS!\n");
+        }
+    } else{
+        printf("Digite m ou f!\n");
     }
+    
 
     return 0;
 }
