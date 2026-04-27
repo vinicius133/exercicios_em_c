b6b7087b297fd67113f648583a3bf3e03a0d1296
Vinícius <viniciusqs13@gmail.com>
2026-04-27 15:56:05 -0300

    programa inicio do estudo de repetição

---
+++ atividades_em_sala/2704/progwhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int numero = 1;
+    while (numero <= 10)
+    {
+        printf("%d ", numero);
+        numero = numero + 1;
+    }
+    
+
+    return 0;
+}
