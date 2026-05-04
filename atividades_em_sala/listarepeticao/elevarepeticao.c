d737af36266deea37d13ec09e5507f0d6b6ff664
Vinícius <viniciusqs13@gmail.com>
2026-05-04 16:20:57 -0300

    terminado

--- atividades_em_sala/listarepeticao/elevarepeticao.c
+++ atividades_em_sala/listarepeticao/elevarepeticao.c
@@ -5,18 +5,18 @@ int main(){
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    int a, b, pot;
+    int a, b, resultado = 1;
     printf("Digite a base a: ");
     scanf("%d", &a);
 
     printf("Digite o expoente b: ");
     scanf("%d", &b);
 
-    while(b > 1){
-        b = b - 1;
-
+    for(int i = 0; i < b; i++){
+        resultado = resultado * a;
     }
 
+    printf("Resultado: %d\n", resultado);
 
 
     return 0;
