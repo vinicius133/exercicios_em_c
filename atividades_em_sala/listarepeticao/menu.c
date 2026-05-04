1e14c6bd0c6322bed36f76c15e6fbd81381f1b61
Vinícius <viniciusqs13@gmail.com>
2026-05-04 15:55:37 -0300

    menu

--- atividades_em_sala/listarepeticao/menu.c
+++ atividades_em_sala/listarepeticao/menu.c
@@ -5,7 +5,38 @@ int main(){
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    printf("MENU\n1 - Rabanada\n2 - Strogonnoff\n3 - Risoto\n4 - Scarggott\n5 - Sair");
+    int opcao = 0;
+   
+    while (opcao != 5) {
+        printf("\nMENU\n");
+        printf("1 - Rabanada\n");
+        printf("2 - Strogonoff\n");
+        printf("3 - Risoto\n");
+        printf("4 - Escargot\n");
+        printf("5 - Sair\n");
 
+        printf("Escolha a opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1:
+                printf("Você escolheu Rabanada!\n");
+                break;
+            case 2:
+                printf("Você escolheu Strogonoff!\n");
+                break;
+            case 3:
+                printf("Você escolheu Risoto!\n");
+                break;
+            case 4:
+                printf("Você escolheu Escargot!\n");
+                break;
+            case 5:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+        }
+    }
     return 0;
 }
