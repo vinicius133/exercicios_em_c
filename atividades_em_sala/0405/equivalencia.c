d20229aebf529d3cddceebc36989cfb4d22b1c65
Vinícius <viniciusqs13@gmail.com>
2026-05-04 14:24:28 -0300

    for iniciado

--- atividades_em_sala/0405/equivalencia.c
+++ atividades_em_sala/0405/equivalencia.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <windows.h>
+// #include <stdio.h>
+// #include <windows.h>
 
-int main() {
-    SetConsoleOutputCP(65001);
-    SetConsoleCP(65001);
+// int main() {
+//     SetConsoleOutputCP(65001);
+//     SetConsoleCP(65001);
 
-    printf("Provaremos a equivalência checando se os valores serão iguais.");
+//     printf("Provaremos a equivalência checando se os valores serão iguais.");
 
-    int numero = 24;
-    numero = numero + 1;
-    //nao terminado
-    numero = numero - 1;
+//     int numero = 24;
+//     numero = numero + 1;
+//     //nao terminado
+//     numero = numero - 1;
 
-    out2 = ++numero;
+//     out2 = ++numero;
 
-    out3 = numero++;
+//     out3 = numero++;
 
-    if (out1 == out2 && out1 == out3 && out2 == out3){
-        printf("%d %d %d, os valores são iguais!", out1, out2, out3);
-    }
+//     if (out1 == out2 && out1 == out3 && out2 == out3){
+//         printf("%d %d %d, os valores são iguais!", out1, out2, out3);
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
