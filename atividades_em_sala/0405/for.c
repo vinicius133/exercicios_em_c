e4a50075dfc5dc4064b60194767d562162085c10
Vinícius <viniciusqs13@gmail.com>
2026-05-04 14:26:28 -0300

    finalizado

--- atividades_em_sala/0405/for.c
+++ atividades_em_sala/0405/for.c
@@ -5,7 +5,10 @@ int main() {
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    
+    int numero;
+    for (numero = 1; numero <= 10; numero++){
+        printf("%d ", numero);
+    }
 
     return 0;
 }
