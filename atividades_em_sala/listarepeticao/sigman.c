a2fae5be5ef5b21ac4d1720dcb9cd399d450e50c
Vinícius <viniciusqs13@gmail.com>
2026-05-17 11:30:21 -0300

    repetição com base em sigma finalizada

--- atividades_em_sala/listarepeticao/sigman.c
+++ atividades_em_sala/listarepeticao/sigman.c
@@ -2,11 +2,14 @@
 
 int main(){
 
-    int n;
+    int n, soma = 0;
     printf("Digite o valor de n: ");
     scanf("%d", &n);
 
-    
+    for (int i = 1; i <= n; i++) {
+        soma = soma + i; 
+    }
+    printf("O resultado da soma é: %d\n", soma);
 
     return 0;
 }
