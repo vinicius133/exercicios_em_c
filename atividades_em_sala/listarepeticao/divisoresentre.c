1da8f9af3ede783bd1c284da914426c13c410424
Vinícius <viniciusqs13@gmail.com>
2026-05-17 12:33:52 -0300

    aperfeiçoamento e finalização

--- atividades_em_sala/listarepeticao/divisoresentre.c
+++ atividades_em_sala/listarepeticao/divisoresentre.c
@@ -7,12 +7,13 @@ int main(){
     printf("Digite o valor de n: ");
     scanf("%d", &n);
 
+    printf("Os divisores entre 2 e %d sao: ", n);
     for(int i = 2; i <= n; i++){
         if (n % i == 0){
             printf("%d ", i);
         }
-        
     }
+    printf("\n");
 
     return 0;
 }
