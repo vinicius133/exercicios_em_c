77cf99d5f89bdfa51a8a2e689cb0ebe702961025
Vinícius <viniciusqs13@gmail.com>
2026-05-17 22:42:00 -0300

    teste do programa polido

--- atividades_em_sala/listarepeticao/matricula.c
+++ atividades_em_sala/listarepeticao/matricula.c
@@ -2,14 +2,22 @@ int main() {
 
     int D = 8, x = 5 + D, y = 0;
 
-    do {
+    do{
+
     y = (x % 2) + 10 * y;
     x = x / 2;
+
     printf("x = %d, y = %d\n", x, y );
+
     } while (x != 0);
+
     while (y != 0) {
+
     x = y % 100;
     y = y / 10;
+
     printf("x = %d, y = %d\n", x, y );
+
     }
+    return 0;
 }
