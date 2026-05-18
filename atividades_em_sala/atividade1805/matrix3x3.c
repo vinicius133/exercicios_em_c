171eb95d50930a15a8f7406aa8f1348fdd3f18c6
Vinícius <viniciusqs13@gmail.com>
2026-05-18 14:47:51 -0300

    matrix 3x3 maior e menor

--- atividades_em_sala/atividade1805/matrix3x3.c
+++ atividades_em_sala/atividade1805/matrix3x3.c
@@ -4,19 +4,35 @@
 
 int main(){
 
-    float m[linha][coluna], soma = 0;
+    float m[linha][coluna], soma = 0, menor, maior;
 
     printf("---Digite os valores da matriz a seguir---\n");
 
     for(int i = 0; i < linha; i++){
         for(int j = 0; j < coluna; j++){
+
             printf("Digite o valor da posicao [%d][%d]: ", i, j);
             scanf("%f", &m[i][j]);
-             soma = soma + m[i][j];
+
+            soma = soma + m[i][j];
+
         }
     }
+    menor = m[0][0];
+    maior = m[0][0];
+    for(int i = 0; i < linha; i++){
+        for(int j = 0; j < coluna; j++){
+            if (menor > m[i][j]){
+                        menor = m[i][j];
+                    }
+                    if (maior < m[i][j]){
+                        maior = m[i][j];
+                    }
+        }
+    }
+    
 
-    printf("\nA soma e: %.2f \n", soma);
+    printf("\nA soma e: %.2f \nO maior e: %.2f\nO menor e: %.2f\n", soma, maior, menor);
 
 
     return 0;
