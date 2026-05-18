0f02dbfdc9a93cedfd0536fae0fa97b26a64c603
Vinícius <viniciusqs13@gmail.com>
2026-05-18 15:31:43 -0300

    matriz 4x4 finalizada

--- atividades_em_sala/atividade1805/2matriz4x4.c
+++ atividades_em_sala/atividade1805/2matriz4x4.c
@@ -4,19 +4,26 @@
 
 int main(){
 
-    int m[linha][coluna], pares;
+    int m[linha][coluna], pares_index = 0, pares[pares_index];
+
     for(int i = 0; i < linha; i++){
         for(int j = 0; j < coluna; j++){
+
             printf("\nDigite o valor da posicao [%d][%d]: \n", i, j);
             scanf("%d", &m[i][j]);
 
             if (m[i][j] % 2 == 0){
-                ;
+                pares[pares_index] = m[i][j];
+                pares_index++;
             }
         }
     }
-        
-            
+    
+    printf("o vetor de numeros pares e: ");
+    for(int i = 0; i < pares_index; i++){
+        printf("%d ", pares[i] );
+    }
+
 
     return 0;
 }
